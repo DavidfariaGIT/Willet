37a28524856715dfc9e588b2cfad40e877b7c416
DavidfariaGIT <davidfaria17@gmail.com>
2025-04-11 14:59:35 -0400

    pls

--- src/ImageTwo.jsx
+++ src/ImageTwo.jsx
@@ -1,20 +1,20 @@
 
-function ImageTwo () {
+function ImageTwo() {
   return (
     <>
-    <section className="bg-orange-50 h-160 lg:h-260">
-    <div className="flex w-full mt-127 md:mt-183 lg:mt-297">
-  <div className="card bg-base-300 rounded-box grid h-20 place-items-center"></div>
-  <img className="w-1/3" src='/src/assets/7f5d941574e28349ff2b7e76975e1c1a69ad43a0-2400x3000.avif'/>
-  <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">
-  <img className="w-2/2" src='/src/assets/67c7c3592f3f36904e5a09f81b572c4f302ceab3-2400x3000.avif' /></div>
-</div>
-<div className="bg-orange-50 mt-54 flex flex-row justify-start">
-  <a className="btn ml-60  btn-m btn-ghost text-red-950 font-extrabold md:ml-0 md:mt-30 lg:mt-117 hover:none border-none shadow-none bg-orange-50 lg:text-lg">WT-G1</a>
-  <a className="btn btn-m btn-ghost text-red-950 font-extrabold md:mt-30 lg:mt-117 hover:bg-orange-50  hover:none border-none shadow-none lg:text-lg">Radi Table and Chairs
-  </a>
-  </div>
-</section>
+      <section className="bg-orange-50 h-160 lg:h-260">
+        <div className="flex w-full mt-127 md:mt-183 lg:mt-297">
+          <div className="card bg-base-300 rounded-box grid h-20 place-items-center"></div>
+          <img className="w-1/3" src='/src/assets/7f5d941574e28349ff2b7e76975e1c1a69ad43a0-2400x3000.jpeg' />
+          <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">
+            <img className="w-2/2" src='/src/assets/67c7c3592f3f36904e5a09f81b572c4f302ceab3-2400x3000.jpeg' /></div>
+        </div>
+        <div className="bg-orange-50 mt-54 flex flex-row justify-start">
+          <a className="btn ml-60  btn-m btn-ghost text-red-950 font-extrabold md:ml-0 md:mt-30 lg:mt-117 hover:none border-none shadow-none bg-orange-50 lg:text-lg">WT-G1</a>
+          <a className="btn btn-m btn-ghost text-red-950 font-extrabold md:mt-30 lg:mt-117 hover:bg-orange-50  hover:none border-none shadow-none lg:text-lg">Radi Table and Chairs
+          </a>
+        </div>
+      </section>
     </>
   )
 }
